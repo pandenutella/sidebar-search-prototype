f15bedc4b05db5a3c36b954f2332ca916a71b872
johnemmanuelb <johnemmanuel.bacalla@gmail.com>
2022-10-18 16:16:28 +0800

    Add dummy data for groups and items

---
+++ src/data/groups.js
@@ -0,0 +1,7 @@
+const groups = [
+  { id: "G001", name: "Group 1" },
+  { id: "G002", name: "Group 2" },
+  { id: "G003", name: "Group 3" },
+];
+
+export default groups;
---
+++ src/data/items.js
@@ -0,0 +1,29 @@
+const items = [
+  {
+    id: "I001",
+    group: "G001",
+    name: "Item 1",
+  },
+  {
+    id: "I002",
+    group: "G001",
+    name: "Item 2",
+  },
+  {
+    id: "I003",
+    group: "G002",
+    name: "Item 3",
+  },
+  {
+    id: "I004",
+    group: "G002",
+    name: "Item 4",
+  },
+  {
+    id: "I005",
+    group: "G003",
+    name: "Item 5",
+  },
+];
+
+export default items;
